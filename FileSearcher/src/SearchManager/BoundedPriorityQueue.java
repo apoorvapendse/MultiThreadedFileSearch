92a8fdda2992b223fd0bca90db73eae636aadc58
Rajeev Tapadia <rtapadia730@gmail.com>
2024-06-11 13:37:36 +0530

    Fixed length BoundedPriorityQueue

---
+++ FileSearcher/src/SearchManager/BoundedPriorityQueue.java
@@ -0,0 +1,40 @@
+package SearchManager;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class BoundedPriorityQueue<T> implements Iterable<T> {
+    PriorityQueue<T> minHeap;
+    int maxSize;
+
+    BoundedPriorityQueue(int maxSize, Comparator<T> comp) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("size must be greater than 0");
+        }
+        this.maxSize = maxSize;
+        // Comparator is reversed as least relevant element is kept at the start of the queue
+        minHeap = new PriorityQueue<>(maxSize, comp.reversed());
+    }
+
+    public void offer(T element) {
+        // if size is less than maxSize then remove the least relevant element
+        if (size() == maxSize) {
+            minHeap.poll();
+        }
+        minHeap.offer(element);
+    }
+
+    public int size() {
+        return minHeap.size();
+    }
+
+    public boolean isEmpty() {
+        return minHeap.isEmpty();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return minHeap.iterator();
+    }
+}
